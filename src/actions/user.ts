625c7aae0f94b514f315ae2edc91446b44874469
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-05-25 22:44:49 +0530

    feat: implement GlobalCard and Modal components for UI consistency

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -95,7 +95,209 @@ export const getNotifications = async () => {
       return { status: 200, data: notifications };
     return { status: 404, data: [] };
   } catch (error) {
-    console.error("Error fetching notifications:", error);
+    console.log("🔴 ERROR", error);
     return { status: 400, data: [] };
   }
 };
+
+export const searchUsers = async (query: string) => {
+  try {
+    const user = await currentUser();
+    if (!user) return { status: 404 };
+
+    const users = await client.user.findMany({
+      where: {
+        OR: [
+          { firstname: { contains: query } },
+          { email: { contains: query } },
+          { lastname: { contains: query } },
+        ],
+        NOT: [{ clerkid: user.id }],
+      },
+      select: {
+        id: true,
+        subscription: {
+          select: {
+            plan: true,
+          },
+        },
+        firstname: true,
+        lastname: true,
+        image: true,
+        email: true,
+      },
+    });
+
+    if (users && users.length > 0) {
+      return { status: 200, data: users };
+    }
+
+    return { status: 404, data: undefined };
+  } catch (error) {
+    console.log("🔴 ERROR", error);
+    return { status: 500, data: undefined };
+  }
+};
+
+export const getPaymentInfo = async () => {
+  try {
+    const user = await currentUser();
+    if (!user) return { status: 404 };
+
+    const payment = await client.user.findUnique({
+      where: {
+        clerkid: user.id,
+      },
+      select: {
+        subscription: {
+          select: { plan: true },
+        },
+      },
+    });
+    if (payment) {
+      return { status: 200, data: payment };
+    }
+  } catch (error) {
+    console.log("🔴 ERROR", error);
+    return { status: 400 };
+  }
+};
+
+export const enableFirstView = async (state: boolean) => {
+  try {
+    const user = await currentUser();
+
+    if (!user) return { status: 404 };
+
+    const view = await client.user.update({
+      where: {
+        clerkid: user.id,
+      },
+      data: {
+        firstView: state,
+      },
+    });
+
+    if (view) {
+      return { status: 200, data: "Setting updated" };
+    }
+  } catch (error) {
+    console.log("🔴 ERROR", error);
+    return { status: 400 };
+  }
+};
+
+export const getFirstView = async () => {
+  try {
+    const user = await currentUser();
+    if (!user) return { status: 404 };
+    const userData = await client.user.findUnique({
+      where: {
+        clerkid: user.id,
+      },
+      select: {
+        firstView: true,
+      },
+    });
+    if (userData) {
+      return { status: 200, data: userData.firstView };
+    }
+    return { status: 400, data: false };
+  } catch (error) {
+    console.log("🔴 ERROR", error);
+    return { status: 400 };
+  }
+};
+
+export const createCommentAndReply = async (
+  userId: string,
+  comment: string,
+  videoId: string,
+  commentId?: string | undefined
+) => {
+  try {
+    if (commentId) {
+      const reply = await client.comment.update({
+        where: {
+          id: commentId,
+        },
+        data: {
+          reply: {
+            create: {
+              comment,
+              userId,
+              videoId,
+            },
+          },
+        },
+      });
+      if (reply) {
+        return { status: 200, data: "Reply posted" };
+      }
+    }
+
+    const newComment = await client.video.update({
+      where: {
+        id: videoId,
+      },
+      data: {
+        Comment: {
+          create: {
+            comment,
+            userId,
+          },
+        },
+      },
+    });
+    if (newComment) return { status: 200, data: "New comment added" };
+  } catch (error) {
+    console.log("🔴 ERROR", error);
+    return { status: 400 };
+  }
+};
+
+export const getUserProfile = async () => {
+  try {
+    const user = await currentUser();
+    if (!user) return { status: 404 };
+    const profileIdAndImage = await client.user.findUnique({
+      where: {
+        clerkid: user.id,
+      },
+      select: {
+        image: true,
+        id: true,
+      },
+    });
+
+    if (profileIdAndImage) return { status: 200, data: profileIdAndImage };
+  } catch (error) {
+    console.log("🔴 ERROR", error);
+    return { status: 400 };
+  }
+};
+
+export const getVideoComments = async (Id: string) => {
+  try {
+    const comments = await client.comment.findMany({
+      where: {
+        OR: [{ videoId: Id }, { commentId: Id }],
+        commentId: null,
+      },
+      include: {
+        reply: {
+          include: {
+            User: true,
+          },
+        },
+        User: true,
+      },
+    });
+
+    return { status: 200, data: comments };
+  } catch (error) {
+    console.log("🔴 ERROR", error);
+    return { status: 400 };
+  }
+};
+
