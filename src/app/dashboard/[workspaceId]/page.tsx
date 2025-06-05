83fb71bf7524868f2aa7327f480924a32e14c1fe
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-06-05 15:39:18 +0530

    feat: add GlobalHeader to Layout and enhance Page component with tabs for better navigation

--- src/app/dashboard/[workspaceId]/page.tsx
+++ src/app/dashboard/[workspaceId]/page.tsx
@@ -1,9 +1,56 @@
+import { getWorkspaceFolders } from "@/actions/workspace";
+import CreateForlders from "@/components/global/create-folders";
+import CreateWorkspace from "@/components/global/create-workspace";
+import Folders from "@/components/global/folders";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { QueryClient } from "@tanstack/react-query";
 import React from "react";
 
-type Props = {};
+type Props = {
+  params: {
+    workspaceId: string;
+  };
+};
 
-const Page = (props: Props) => {
-  return <div></div>;
+const Page = async ({ params: { workspaceId } }: Props) => {
+  const query = new QueryClient();
+
+  await query.prefetchQuery({
+    queryKey: ["workspace-folders"],
+    queryFn: () => getWorkspaceFolders(workspaceId),
+  });
+
+  return (
+    <div>
+      <Tabs defaultValue="videos" className="mt-6">
+        <div className="flex w-full justify-between items-center">
+          <TabsList className="bg-transparent gap-2 pl-0">
+            <TabsTrigger
+              className="p-[13px] px-6 rounded-full data-[state=active]:bg-[#252525]"
+              value="videos"
+            >
+              Videos
+            </TabsTrigger>
+            <TabsTrigger
+              value="archive"
+              className="p-[13px] px-6 rounded-full data-[state=active]:bg-[#252525]"
+            >
+              Archive
+            </TabsTrigger>
+          </TabsList>
+          <div className="flex gap-x-3">
+            <CreateWorkspace />
+            <CreateForlders workspaceId={workspaceId} />
+          </div>
+        </div>
+        <section className="py-9">
+          <TabsContent value="videos">
+            <Folders workspaceId={workspaceId} />
+          </TabsContent>
+        </section>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Page;
